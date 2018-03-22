a2aeaf3c085fed9e8639f20672d20a651d121157
mohammad zohaib <mohammadzohaib804@gmail.com>
2018-03-23 00:44:25 +0500

    second commit

--- code.js
+++ code.js
@@ -10,10 +10,11 @@
         };
         firebase.initializeApp(config);
         var firestore=firebase.firestore();
-        const docRef=firestore.doc("To-Do/To-Do-Data");
-function add() {
+        
+function add2() {
     var li_Element = document.createElement('li'); //creating the li element that will placed in ol element
     var span_Element = document.createElement('span'); //creating the span element that wil placed in li element
+    var div_Element = document.createElement('div');
     var data = document.getElementById('text_Field').value; // todo task
     if (data === "") {
         alert("Please Enter some task to do Empty task is not allowed");
@@ -25,36 +26,41 @@ function add() {
         var add = document.getElementById('bottom'); //it gives ul element <ul id="bottom"></ul>
         var delete_Btn = document.createElement('input'); //create delete button
         var update_Btn = document.createElement('input'); //create update button
-        var Done_Btn = document.createElement('input'); //create done button
+        var done_Btn = document.createElement('input'); //create done button
         var li_Add = add.appendChild(li_Element); //attach ol to li
-
         li_Add.appendChild(span_Element);
-        li_Add.appendChild(delete_Btn);
-        li_Add.appendChild(update_Btn);
-        li_Add.appendChild(Done_Btn);
+        span_Element.setAttribute("id","span");
+        li_Add.appendChild(div_Element);
+        
+        div_Element.appendChild(delete_Btn);
+        div_Element.appendChild(update_Btn);
+        div_Element.appendChild(done_Btn);
 
-        Done_Btn.setAttribute("type", "submit");
-        Done_Btn.setAttribute("value", "Done");
-        Done_Btn.setAttribute("onClick", "Done()");
+        div_Element.setAttribute("id","wrap_Btn")
+        done_Btn.setAttribute("type", "submit");
+        done_Btn.setAttribute("class", "btn btn-default");
+        done_Btn.setAttribute("value", "Done");
+        done_Btn.setAttribute("onClick", "Done()");
 
         delete_Btn.setAttribute("type", "submit");
+        delete_Btn.setAttribute("class", "btn btn-default");
         delete_Btn.setAttribute("value", "Delete");
         delete_Btn.setAttribute("onClick", "Delete()");
 
         update_Btn.setAttribute("type", "submit");
+        update_Btn.setAttribute("class", "btn btn-default");
         update_Btn.setAttribute("value", "Update");
         update_Btn.setAttribute("onClick", "Update(this)");
         document.getElementById('text_Field').value = " ";
-        var setWithMerge = docRef.set({
+        firestore.collection("To-Do").add({
             task:data
         })
-        .then(function()
-            {
-            console.log("status saved...")
-            }).catch(function(error)
-            {
-                console.log(error);
-            });
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
         
     }
 }
